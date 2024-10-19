b712cdd71951793911e44ce59b35b6a7bfd1914f
Aryan Chawla <aryanchawlad@gmail.com>
2024-10-18 21:17:10 -0400

    Pieces Creation

--- chessboard/board.py
+++ chessboard/board.py
@@ -4,11 +4,8 @@ import pygame
 
 
 class Board:
-   """
-      Constructor for board
-      param:
-         flipped - determine whether board should be flipped
-   """
+
+
    def __init__(self) -> None:
       self.width = 2000
       self.height = 800
@@ -17,10 +14,48 @@ class Board:
       self.font = None
       self.fps = 60
 
+      self.board1 = [
+         ['br', 'bk', 'bb', 'bq', 'bk', 'bb', 'bk', 'br'],
+         ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+         ['wr', 'wk', 'wb', 'wq', 'wk', 'wb', 'wk', 'wr']
+      ]
+      self.board2 = [
+         ['wr', 'wk', 'wb', 'wk', 'wq', 'wb', 'wk', 'wr'],
+         ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['', '', '', '', '', '', '', ''],
+         ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+         ['br', 'bk', 'bb', 'bk', 'bq', 'bb', 'bk', 'br'],
+      ]
+
+      self.image_loader = {
+         'wp': './chessboard/images/Chess_plt60.png',
+         'bp': './chessboard/images/Chess_pdt60.png',
+         'wr': './chessboard/images/Chess_rlt60.png',
+         'br': './chessboard/images/Chess_rdt60.png',
+         'wk': './chessboard/images/Chess_nlt60.png',
+         'bk': './chessboard/images/Chess_ndt60.png',
+         'wb': './chessboard/images/Chess_blt60.png',
+         'bb': './chessboard/images/Chess_bdt60.png',
+         'wk': './chessboard/images/Chess_klt60.png',
+         'bk': './chessboard/images/Chess_kdt60.png',
+         'wq': './chessboard/images/Chess_qlt60.png',
+         'bq': './chessboard/images/Chess_qdt60.png'
+      }
+
+
       self.create_window()
 
 
    def draw_board(self):
+      # chessboard
       for j in range(8):
          for i in range(8):
             color1 = 'white' if (i+j) % 2 == 0 else 'brown'
@@ -28,6 +63,17 @@ class Board:
             pygame.draw.rect(self.screen, color1, [100 * i, 100*j, 100, 100])
             pygame.draw.rect(self.screen, color2, [100 * i + 1200, 100*j, 100, 100])
 
+      # pieces
+      for j in range(8):
+         for i in range(8):
+            if self.board1[j][i]:
+               piece = self.image_loader[self.board1[j][i]]
+               load = pygame.image.load(piece).convert_alpha()
+               self.screen.blit(load, (100*i + 20, 100 * j + 20))
+            if self.board2[j][i]:
+               piece = self.image_loader[self.board2[j][i]]
+               load = pygame.image.load(piece).convert_alpha()
+               self.screen.blit(load, (100*i + 1220, 100 * j + 20))
 
 
    def create_window(self):
@@ -37,7 +83,6 @@ class Board:
       self.screen = pygame.display.set_mode([self.width, self.height])
       pygame.display.set_caption('Chess Board')
       self.timer = pygame.time.Clock()
-
 
       run = True
       while run:
@@ -50,4 +95,3 @@ class Board:
                run = False
          
          pygame.display.flip()
-         
